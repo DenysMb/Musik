b9ab8795fdaf1fd6e406c5f7e7892bfd23cf274e
Denys Madureira <denys.madureira@pm.me>
2026-01-12 20:54:33 +0000

    Feat: Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,35 @@
+#include <QApplication>
+#include <KIconTheme>
+#include <KLocalizedString>
+#include <QQmlApplicationEngine>
+#include <QUrl>
+#include <QQuickStyle>
+#include <QQmlContext>
+#include <QQmlEngine>
+
+#include <KLocalizedContext>
+
+int main(int argc, char *argv[])
+{
+    KIconTheme::initTheme();
+    QApplication app(argc, argv);
+    KLocalizedString::setApplicationDomain("musik");
+    QApplication::setApplicationName(QStringLiteral("Musik"));
+    QApplication::setDesktopFileName(QStringLiteral("io.github.denysmb.musik.desktop"));
+    QApplication::setStyle(QStringLiteral("breeze"));
+    if (qEnvironmentVariableIsEmpty("QT_QUICK_CONTROLS_STYLE")) {
+        QQuickStyle::setStyle(QStringLiteral("org.kde.desktop"));
+    }
+
+    QQmlApplicationEngine engine;
+    QQmlContext ctx(&engine);
+    ctx.setContextObject(new KLocalizedContext(&engine));
+
+    engine.loadFromModule("io.github.denysmb.musik", "Main");
+
+    if (engine.rootObjects().isEmpty()) {
+        return -1;
+    }
+
+    return app.exec();
+}
